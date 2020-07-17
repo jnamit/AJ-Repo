81562c325323b69cb35e13c2b77b3ddda8fd0ab7
jnamit <32260306+jnamit@users.noreply.github.com>
2020-07-17 18:16:51 +0530

    Create OS_Commands.py

---
+++ Python-Advanced/OS_Commands.py
@@ -0,0 +1,11 @@
+import os
+
+#os.system('ls -l')
+os.system("dir")
+
+# But using above cmd you don;t get handle to the output it gave.
+#So, use the following
+
+stream = os.popen("echo 'Amit Jain'")
+output = stream.read()
+print(output)
